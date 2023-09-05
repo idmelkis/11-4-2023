88d0f6c70998a9a36e58b38bf989a104cf50e6f6
Ingmārs Meļķis <122742982+idmelkis@users.noreply.github.com>
2023-09-05 10:09:15 +0300

    Pirmā stunda
    Ievads pitonā #1

---
+++ 1_stunda.py
@@ -0,0 +1,19 @@
+# nosaukums = vērtība
+print("11-4")
+
+x = 100
+print(x)
+
+y = "11-4"
+print(y)
+
+c = "x = "
+# Alternatīva: c += x
+c = c + str(x) + " " + str(5) + "asdas" + y
+print(c)
+
+#print(f"{x}")
+
+# File -> New File 
+# Save As -> nosaukums.py
+# Lai palaistu kodu spiežat pogu fn+F5 uz klaviatūras
