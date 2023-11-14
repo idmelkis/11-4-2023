d79cc5a38239b3ad2200a062cac9e25320c68636
Ingmars Melkis <idmelkis@edu.riga.lv>
2023-11-14 09:06:29 +0200

    14.11 uzdevums

---
+++ 8_nodarbiba.py
@@ -0,0 +1,14 @@
+# Uzdevums: Uzrakstīt algas kalkulatoru (gadam)
+# LV Algas aprēķina apraksts pieejams:
+# https://kalkulatori.lv/lv/algas-kalkulators
+# Vairāk par IIN: https://www.vid.gov.lv/lv/iedzivotaju-ienakuma-nodokla-likmes 
+# Soc. Nodoklis: https://www.vid.gov.lv/lv/valsts-socialas-apdrosinasanas-obligato-iemaksu-likmes
+
+# Jāņem vērā: Neapliekamais minimums
+# IIN: 
+# ienākumam līdz 20 004 eiro – 20 %;
+# ienākumam no 20 004 eiro līdz 78 100 eiro – 23 %;
+# ienākuma daļai, kas pārsniedz 78 100 eiro – 31 %.
+# Sociālais nodoklis:
+# Darba devējs maksā: 23,59% no algas
+# Darba ņēmējs maksā: 10,50% no algas
