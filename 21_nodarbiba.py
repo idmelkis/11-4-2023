f302c5f46d72731b6f5f5dae4c1deb2b921d1061
Ingmars Melkis <idmelkis@edu.riga.lv>
2024-04-02 09:22:25 +0300

    02.04 kods - 2.daļa

--- 21_nodarbiba.py
+++ 21_nodarbiba.py
@@ -24,7 +24,57 @@
 #     lietotajs_id  : int
 
 # Indeksi: https://www.geeksforgeeks.org/indexing-in-databases-set-1/
+# DB Browser: https://sqlitebrowser.org/
 
+import sqlite3
+
+db = sqlite3.connect('21_nodarbiba.db')
+cur = db.cursor()
+
+# bool = True/False = int 1/0
+# Datu tipi: https://www.sqlite.org/datatype3.html
+# CREATE TABLE <nosaukums> (<kolona> <DATU TIPS>, <kolona> <DATU TIPS>)
+# AUTOINCREMENT - Automātiski palielina vērtību par 1 katrā ierakstā
+# PRIMARY KEY - Speciāla atslēga kuru izmantojam lai paātrinātu datu piekļuvi
+create_db = "CREATE TABLE IF NOT EXISTS Lietotajs (id INTEGER NOT NULL UNIQUE,\
+vards STRING,\
+lietotajvards STRING NOT NULL UNIQUE,\
+parole STRING,\
+vecums INTEGER,\
+PRIMARY KEY(\"id\" AUTOINCREMENT))"
+cur.execute(create_db)
+
+# class Prece:
+#     id            : int
+#     nosaukums     : str
+#     cena          : float # REAL
+#     def __init__(self, id, nos, cena) -> None:
+#         self.id = id
+#         self.nosaukums = nos
+#         self.cena = cena
+create_db = "CREATE TABLE IF NOT EXISTS Prece (id INTEGER NOT NULL UNIQUE,\
+nosaukums STRING,\
+cena REAL,\
+PRIMARY KEY(\"id\" AUTOINCREMENT))"
+cur.execute(create_db)
+
+# l1 = Lietotajs(1, "lietotajs", None, "v1", 33)
+# ===
+# insert_data = "INSERT INTO Lietotajs (vards, lietotajvards, vecums)\
+# VALUES (\"lietotajs\", \"lietotajs\", 33)"
+# cur.execute(insert_data)
+
+# l2 = Lietotajs(4, "skolens", "hunter2", "Skolēns", 16)
+# ===
+# insert_data = "INSERT INTO Lietotajs (vards, lietotajvards, parole, vecums)\
+# VALUES (\"Skolēns\", \"skolens\", \"hunter2\", 33)"
+# cur.execute(insert_data)
+
+#select = "SELECT (id, vards, lietotajvards, parole) FROM "
+select = "SELECT * FROM Lietotajs"
+cur.execute(select)
+
+results = cur.fetchall()
 class Lietotajs:
     id            : int
     lietotajvards : str
@@ -37,22 +87,12 @@ class Lietotajs:
         self.parole = parole
         self.vards = vards
         self.vecums = vecums
+result_objs = []
+for result in results:
+    result_objs.append(Lietotajs(result[0], result[2], result[1], result[3], result[4]))
+print(result_objs[0].lietotajvards)
 
-import sqlite3
-
-db = sqlite3.connect('21_nodarbiba.db')
-# bool = True/False = int 1/0
-# Datu tipi: https://www.sqlite.org/datatype3.html
-# CREATE TABLE <nosaukums> (<kolona> <DATU TIPS>, <kolona> <DATU TIPS>)
-# AUTOINCREMENT - Automātiski palielina vērtību par 1 katrā ierakstā
-# PRIMARY KEY - Speciāla atslēga kuru izmantojam lai paātrinātu datu piekļuvi
-create_db = "CREATE TABLE Lietotajs (id INTEGER AUTOINCREMENT PRIMARY KEY,\
-vards STRING,\
-lietotajvards STRING,\
-parole STRING,\
-vecums INTEGER)"
-cur = db.cursor()
-
+cur.close()
 db.commit()
 db.close()
 
