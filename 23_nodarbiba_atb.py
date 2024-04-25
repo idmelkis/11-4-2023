d10bdc995276bcf6e6cf7b11b3c409d722cbe9e5
Ingmars Melkis <idmelkis@edu.riga.lv>
2024-04-25 09:01:04 +0300

    i/ni darba risinājums

--- 23_nodarbiba_atb.py
+++ 23_nodarbiba_atb.py
@@ -1,7 +1,9 @@
-# i/ni darba risinājums:
 import sqlite3
-db = sqlite3.connect("23_nodarbiba.db")
+db = sqlite3.connect('24_nodarbiba.db')
 cur = db.cursor()
+
+# Ieslēdzam foreign key atbalstu datu bāzē
+cur.execute("PRAGMA foreign_keys = ON;")
 
 cur.execute("CREATE TABLE IF NOT EXISTS autors (\
             id INTEGER UNIQUE,\
@@ -11,35 +13,37 @@ cur.execute("CREATE TABLE IF NOT EXISTS autors (\
             PRIMARY KEY (id AUTOINCREMENT)\
 )")
 
-cur.execute("PRAGMA foreign_keys = ON;")
 cur.execute("CREATE TABLE IF NOT EXISTS ieraksts (\
             id INTEGER UNIQUE,\
-            autors STRING,\
+            autors INTEGER,\
             nosaukums STRING,\
             teksts STRING,\
             izveidots STRING,\
-            redzams INTEGER,\
-            PRIMARY KEY (id AUTOINCREMENT)\
-            FOREIGN KEY (autors) REFERENCES autors(id) ON DELETE CASCADE\
-)")
-cur.execute("CREATE TABLE IF NOT EXISTS komentars (\
-            id INTEGER UNIQUE,\
-            ieraksts STRING,\
-            autors STRING,\
-            teksts STRING,\
-            redzams INTEGER,\
-            PRIMARY KEY (id AUTOINCREMENT)\
-            FOREIGN KEY (ieraksts) REFERENCES ieraksts(id) ON DELETE CASCADE\
+            redzams integer,\
+            PRIMARY KEY (id AUTOINCREMENT),\
+            FOREIGN KEY(autors) REFERENCES autors(id) ON DELETE CASCADE\
 )")
 
-cur.execute("INSERT INTO autors (lietotajvards, parole, epasts) \
-            VALUES\
-            (\"admin\", \"parole\", \"....\"),\
-            (\"...\", \"...\", \"...\")")
-cur.execute("INSERT INTO ieraksts (autors, nosaukums, teksts, redzams) \
-            VALUES\
-            (1, \"Sveika pasaule\", \":)\", 1),\
-            (\"...\", \"...\", \"...\")")
+cur.execute("CREATE TABLE IF NOT EXISTS komentars (\
+            id INTEGER UNIQUE,\
+            ieraksts INTEGER,\
+            autors STRING,\
+            teksts STRING,\
+            redzams integer,\
+            PRIMARY KEY (id AUTOINCREMENT),\
+            FOREIGN KEY(ieraksts) REFERENCES ieraksts(id) ON DELETE CASCADE\
+)")
+
+cur.execute("INSERT INTO autors (lietotajvards, parole, epasts) VALUES\
+            ('admin', 'parole', 'admin@mail.com'),\
+            ('user', 'parole123', 'user@mail.com')")
+cur.execute("INSERT INTO ieraksts (autors, nosaukums, teksts, redzams) VALUES\
+            (1, 'sveika pasaule', ':)', 1),\
+            (2, 'Otrais raksts', ':X', 0)")
+cur.execute("INSERT INTO komentars (ieraksts, autors, teksts, redzams) VALUES\
+            (1, 'Janis', 'Sveiks!', 1),\
+            (1, 'spam', 'buy coin', 0),\
+            (2, 'user', 'Test', 1)")
 
 cur.close()
 db.commit()
